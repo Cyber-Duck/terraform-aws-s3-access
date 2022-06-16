f818367a38cbd9348ef0c3982c51846f4cf04095
Sam Crudge <samuel.crudge@cyber-duck.co.uk>
2022-06-16 14:39:05 +0100

    Update policy

--- policy.tf
+++ policy.tf
@@ -5,21 +5,22 @@ resource "aws_iam_group_policy" "s3_access_policy" {
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   # This 
-  policy = jsonencode({
-    "Version" = "2012-10-17",
-    "Statement" = [
+  policy = <<EOT{
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Sid" = "",
-        "Effect" = "Allow",
-        "Action" =[
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Action" : [
           "s3:ListBucket",
           "s3:GetObject",
           "s3:GetBucketLocation",
         ],
-        "Resource" = [for arn in var.s3.arn : [format("%s%s,", "arn:aws:s3:::", arn )]]
+        "Resource" : [for arn in var.s3.arn : [format("%s%s,", "arn:aws:s3:::", arn )]]
       }
     ]
-  })
+  }
+  EOT
   depends_on = [
     aws_iam_group.s3_access
   ]
