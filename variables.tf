96e0862ef9ac446282d85918d138f775e6398fa2
Sam Crudge <samuel.crudge@cyber-duck.co.uk>
2022-06-19 00:18:35 +0100

    adding nullable variables so it passes validation

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "group_user" {
 variable "iam_group_name" {
   type        = string
   description = "Name of IAM group to be created, e.g 'Drupal-devs'"
-  default     = null
+  nullable = true
   validation {
     condition     = can(regex("^[0-9A-Za-z+=,.@_-]+$", var.iam_group_name))
     error_message = "The var.iam_group_name must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
@@ -29,7 +29,7 @@ variable "iam_group_name" {
 variable "existing_group" {
   type        = string
   description = "Existing IAM Group"
-  default     = null
+  nullable = true
   validation {
     condition     = can(regex("^[0-9A-Za-z+=,.@_-]+$", var.existing_group))
     error_message = "The var.existing_group must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
@@ -48,7 +48,7 @@ variable "s3_arns" {
 variable "additional_policy_arn" {
   type        = map(list(string))
   description = "Policy ARN of any additional policys"
-  default     = null
+  nullable = true
   validation {
     condition     = can(substr(var.additional_policy_arn, 0, 8) == "arn:aws:")
     error_message = "The var.additional_policy_arn value must start with 'arn:aws:' please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_identifiers.html."
