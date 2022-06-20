3c88fca331f548dcfd198deb7091e2d2519f90c2
Samuel Crudge <samuel.crudge@cyber-duck.co.uk>
2022-06-20 23:05:31 +0100

    pinpointing issues

--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "iam_user" {
   type        = map(list(string))
   description = "new user creation, default = {['matt.smith', 'janes.doe']}"
-  validation {
-    condition     = var.iam_user != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.iam_user)) : false
-    error_message = "The var.iam_user must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
-  }
+  # validation {
+  #   condition     = var.iam_user != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.iam_user)) : false
+  #   error_message = "The var.iam_user must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
+  # }
 }
 
 variable "group_user" {
   type        = map(list(string))
   description = "Users to be added to S3 access group, default = {['matt.smith', 'janes.doe']}"
   default     = null
-  validation {
-    condition     = var.group_user != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.group_user)) : false
-    error_message = "The var.group_user must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
-  }
+  # validation {
+  #   condition     = var.group_user != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.group_user)) : false
+  #   error_message = "The var.group_user must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
+  # }
 }
 
 variable "iam_group_name" {
   type        = string
   description = "Name of IAM group to be created, e.g 'Drupal-devs'"
   default     = null
-  validation {
-    condition     = var.iam_group_name != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.iam_group_name)) : false
-    error_message = "The var.iam_group_name must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
-  }
+  # validation {
+  #   condition     = var.iam_group_name != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.iam_group_name)) : false
+  #   error_message = "The var.iam_group_name must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
+  # }
 }
 
 variable "existing_group" {
@@ -32,7 +32,7 @@ variable "existing_group" {
   description = "Existing IAM Group"
   default     = null
   validation {
-    condition     = var.existing_group != null ? can(regex("^[0-9A-Za-z+=,.@_-]+$", var.existing_group)) : false
+    condition     = var.existing_group != null ? regex("^[0-9A-Za-z+=,.@_-]+$", var.existing_group) : null
     error_message = "The var.existing_group must contain valid characters please refer to https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html."
   }
 }
